0dc4dbe912c5314ae36146e72d0b43fdd066abf9
Ferr <ursusmartimus007@gmail.com>
2023-09-06 11:38:23 +0300

    1 and 2.b

--- app/src/main/java/com/example/pr1k/MainActivity.kt
+++ app/src/main/java/com/example/pr1k/MainActivity.kt
@@ -2,14 +2,28 @@ package com.example.pr1k
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.pr1k.databinding.ActivityMainBinding
+import com.example.pr1k.databinding.FragmentMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mainb: FragmentMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        mainb = FragmentMainBinding.
+        mainb.FirstButton.setOnClickListener {
+            val first = FirstFragment()
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.nav_host_fragment, first)
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
     }
+
+
 }
